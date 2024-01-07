11e864e88f056eaa8b938e8851492da32274b033
OHYA Satoshi <conatus.11099@gmail.com>
2024-01-07 13:31:49 +0900

    first commit

---
+++ DPS310_HAL_I2C.h
@@ -0,0 +1,36 @@
+/*
+ * DPS310_HAL_I2C.h
+ *
+ *  Created on: Dec 29, 2023
+ *      Author: ohya
+ */
+
+#ifdef USE_HAL_DRIVER
+
+#ifndef INC_DPS310_HAL_I2C_H_
+#define INC_DPS310_HAL_I2C_H_
+
+#include <DPS310.h>
+
+#include "i2c.h"
+
+class DPS310_HAL_I2C: public DPS310 {
+public:
+	DPS310_HAL_I2C(I2C_HandleTypeDef *hi2c, I2C_ADDRESS address);
+
+	uint8_t readByte(REGISTOR reg);
+	uint8_t readByte(uint8_t reg);
+	void writeByte(REGISTOR reg, uint8_t value);
+	void readBytes(REGISTOR reg, uint8_t *pData, uint8_t size = 1);
+
+	float convertRaw2Pressure(uint8_t rawData[6]);
+	float convertRaw2Pressure(std::array<uint8_t, 6> rawData);
+
+private:
+	I2C_HandleTypeDef *hi2c;
+	uint8_t i2cAddress;
+};
+
+#endif /* INC_DPS310_HAL_I2C_H_ */
+
+#endif /* USE_HAL_DRIVER */
